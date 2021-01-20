29e5c59342e19ce61bc1d5b61ee221cd536b361c
Faly <faly@ibonia.mg>
2021-01-20 09:28:22 +0300

    fix-header

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,21 @@ var app = {
 $("document").ready(function () {
   app.init();
 });
+
+
+
+(function ($) {
+    'use strict';
+/*--------------------------------------------------------
+    / 11. Fixed Header
+    /----------------------------------------------------------*/
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 40)
+        {
+            $(".head-sticky").addClass('fix-header animated slideInDown');
+        } else
+        {
+            $(".head-sticky").removeClass('fix-header animated slideInDown');
+        }
+    });
+})(jQuery);
